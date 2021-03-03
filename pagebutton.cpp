6675cbee5fb1a38ce40c5f60ca0d896f8ed2c8a5
ChiuYeeJay <shes202@gmail.com>
2021-03-03 13:11:47 +0800

    working on page button class 2

--- pagebutton.cpp
+++ pagebutton.cpp
@@ -1,6 +1,12 @@
 #include "pagebutton.h"
 
-PageButton::PageButton()
+PageButton::PageButton(bool def_sel, QIcon icon)
+    :QToolButton()
 {
+    selected = def_sel;
+    setIcon(icon);
+}
+
+void PageButton::change_selected(){
 
 }
--- pagebutton.h
+++ pagebutton.h
@@ -2,15 +2,18 @@
 #define PAGEBUTTON_H
 
 #include <QToolButton>
+#include <QIcon>
 
 class PageButton : public QToolButton
 {
 public:
-    PageButton();
+    PageButton(bool def_sel = false, QIcon icon = QIcon());
 private:
     bool selected;
 public Q_SLOT:
     void change_selected();
+    void mouse_enter();
+    void mouse_leave();
 };
 
 #endif // PAGEBUTTON_H
