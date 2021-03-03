037ac4ae343b598ff00de897b61c0189fb58af0b
ChiuYeeJay <shes202@gmail.com>
2021-03-03 14:57:10 +0800

    working on page button class 3

--- pagebutton.cpp
+++ pagebutton.cpp
@@ -5,8 +5,27 @@ PageButton::PageButton(bool def_sel, QIcon icon)
 {
     selected = def_sel;
     setIcon(icon);
+    btn_list.append(this);
+    connect(this, &PageButton::clicked, &PageButton::someone_clicked);
+}
+
+void PageButton::someone_clicked(PageButton *caller){
+    for(PageButton *pb : btn_list){
+        if(pb == caller) pb->setSelected(true);
+        else pb->setSelected(false);
+    }
 }
 
 void PageButton::change_selected(){
 
+}
+
+void PageButton::setSelected(bool sel){
+    if(selected && !sel){
+
+    }
+    else if(!selected && sel){
+
+    }
+    selected = sel;
 }
--- pagebutton.h
+++ pagebutton.h
@@ -3,17 +3,22 @@
 
 #include <QToolButton>
 #include <QIcon>
+#include <QList>
 
 class PageButton : public QToolButton
 {
 public:
     PageButton(bool def_sel = false, QIcon icon = QIcon());
 private:
+    static QList<PageButton*> btn_list;
     bool selected;
 public Q_SLOT:
     void change_selected();
     void mouse_enter();
     void mouse_leave();
+    static void someone_clicked(PageButton *caller);
+private:
+    void setSelected(bool sel);
 };
 
 #endif // PAGEBUTTON_H
